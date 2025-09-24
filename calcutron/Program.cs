9650dfeb86119600a92613fa6c0678b9e45d947e
Aaron Foley <Aaron Foley@DESKTOP-2NI5895>
2025-09-24 14:49:54 +0100

    me when

--- calcutron/Program.cs
+++ calcutron/Program.cs
@@ -1,10 +1,50 @@
-﻿
-Console.WriteLine("Type in first number ");
-int firstnumber = Convert.ToInt32(Console.ReadLine());
+﻿using System.Xml.Serialization;
 
-Console.WriteLine("type second number"); 
-int secondnumber= Convert.ToInt32(Console.ReadLine());  
+Calculatorapp(); 
 
-int result = firstnumber +  secondnumber;
-Console.WriteLine("adding {0} and {1} give the answer {2}", firstnumber, secondnumber, result); 
+void Calculatorapp()
+{
+    int firstnumber = 0;
+    int secondnumber = 0;
+    int result = 0;
+    int choice = 0; 
+    Console.WriteLine("Type in first number ");
+     firstnumber = Convert.ToInt32(Console.ReadLine());
 
+    Console.WriteLine("type second number");
+     secondnumber = Convert.ToInt32(Console.ReadLine());
+
+    Console.WriteLine("Choose an option");
+    Console.WriteLine("1  - add");
+    Console.WriteLine("2 - subtract");
+    Console.WriteLine("3 - divide");
+    Console.WriteLine("4 - mult");
+
+    choice = Convert.ToInt32(Console.ReadLine());
+
+    if (choice == 1)
+    {
+
+        result = firstnumber + secondnumber;
+        Console.WriteLine($"adding {firstnumber} and {secondnumber} equals {result}");
+    }
+    if (choice == 2)
+    {
+        result = firstnumber - secondnumber;
+        Console.WriteLine($"subtracting {firstnumber} and {secondnumber} equals {result} ");
+    }
+    if (choice == 3)
+    {
+        result = firstnumber / secondnumber;
+        Console.WriteLine($"dividing {firstnumber} by {secondnumber} equals {result}");
+    }
+    if (choice == 4)
+    {
+        result = firstnumber * secondnumber;
+        Console.WriteLine($"multplying {firstnumber}by  {secondnumber} equals {result}");
+    }
+    else
+    {
+        Console.WriteLine("you did not enter right number");
+    }
+}
