0d5df48b9d5c3f7c1ed69fd5a5e9d162e72faae2
Aaron Foley <Aaron Foley@DESKTOP-2NI5895>
2025-09-24 14:22:30 +0100

    the note

--- calcutron/Program.cs
+++ calcutron/Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+Console.WriteLine("Type in first number ");
+int firstnumber = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("type second number"); 
+int secondnumber= Convert.ToInt32(Console.ReadLine());  
+
+int result = firstnumber +  secondnumber;
+Console.WriteLine("adding {0} and {1} give the answer {2}", firstnumber, secondnumber, result); 
+
